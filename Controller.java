3626c66162c5d717a006f030e8d7400a519eb7db
Will Krause <wkrause1@radford.edu>
2018-11-06 21:57:14 -0500

    WIP for removing file

--- Controller.java
+++ Controller.java
@@ -1,6 +1,5 @@
 package WCThread;
 
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -16,7 +15,6 @@ public class Controller {
     @FXML
     private ListView listViewFiles;
     private ObservableList<String> files = FXCollections.observableArrayList();
-    private FileChooser fileChooser;
     private Stage stage;
     private List<File> fileList;
     private List <String> filePaths;
@@ -31,25 +29,47 @@ public class Controller {
     }
 
     private void makeFilePathList() {
-        for (File f : fileList) {
-            filePaths.add(f.getPath());
+        if (!fileList.isEmpty()) {
+            for (File f : fileList) {
+                filePaths.add(f.getPath());
+            }
         }
     }
 
     public void showFileChooser() {
-        fileChooser = new FileChooser();
+        FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("Text Files", "*.txt"));
         fileList = fileChooser.showOpenMultipleDialog(stage);
         makeFilePathList();
-        mamkeFileNameList();
+        makeFileNameList();
         fillListView();
     }
 
-    private void mamkeFileNameList() {
-        for (File f : fileList) {
-            fileNames.add(f.getName());
+    private void makeFileNameList() {
+        if(!fileList.isEmpty()) {
+            for (File f : fileList) {
+                fileNames.add(f.getName());
+            }
         }
+    }
+
+    public void removeFile() {
+        int index = listViewFiles.getSelectionModel().getSelectedIndex();
+        listViewFiles.getItems().remove(index);
+        String s = files.get(index);
+        System.out.println(s);
+        for (String str: filePaths) {
+            if(str.contains(s)) {
+                filePaths.remove(str);
+            }
+        }
+        for (String str: fileNames) {
+            if (str.contains(s)) {
+                fileNames.remove(str);
+            }
+        }
+
     }
 
     public void setStage(Stage stage) {
--- Counter.java
+++ Counter.java
@@ -1,5 +1,6 @@
 package WCThread;
 
+import org.apache.commons.io.FileUtils;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
@@ -7,13 +8,16 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Counter implements Runnable {
     private String filePath;
+    private String fileText;
     private File file;
     private int wordCount;
+    private int charCount;
     private Stage newStage;
     private TextArea tx;
     private final int WIDTH = 1280;
@@ -24,6 +28,7 @@ public class Counter implements Runnable {
         this.filePath = filePath;
         file = new File(filePath);
         wordCount = 0;
+        charCount = 0;
         initGUI();
 
     }
@@ -41,17 +46,29 @@ public class Counter implements Runnable {
 
     private void updateGUI() {
         tx.appendText("Number of words: " + wordCount);
+        tx.appendText("\nNumber of characters: " + charCount + "\n\n");
+        tx.appendText("Contents of file:\n");
+        tx.appendText(fileText);
     }
 
     private void countWords() {
         try(Scanner sc = new Scanner(new FileInputStream(file))){
+            fileText = FileUtils.readFileToString(file);
             while(sc.hasNext()) {
-                sc.next();
+                String s = sc.next();
+                s.toLowerCase();
                 wordCount++;
+                for (int i = 0; i<s.length(); i++) {
+                    if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z'){
+                        charCount++;
+                    }
+                }
             }
         }
         catch(FileNotFoundException e) {
 
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
